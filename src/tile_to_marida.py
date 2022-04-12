69e0c456edf5b51cf34f90915bc51e5ca940ff3f
Pierre Tessier <pierre.borie.tessier@gmail.com>
2022-04-11 21:22:28 -0400

    add feature: --overwrite + fix bug with empty dataframe

--- src/tile_to_marida.py
+++ src/tile_to_marida.py
@@ -9,12 +9,14 @@ from outliers_pipeline.plasticfinder.data_processing import pre_processing_visua
 from outliers_pipeline.plasticfinder.data_querying import pre_process_tile
 from outliers_pipeline.plasticfinder.utils import get_matching_marida_target
 
+
 if __name__ == "__main__":
     parser = ArgumentParser(
         description="This script finds all the tiles in .tiff format in a directory, that are matching a MARIDA scene. It then computes their outliers, the matching pixels with MARIDA, and wirtes both datasets.")
     parser.add_argument("-t", type=str, help="Tiles directory")
     parser.add_argument("-m", type=str, help="MARIDA directory")
     parser.add_argument("-o", type=str, help="output directory")
+    parser.add_argument("--overwrite", action="store_true")
     args = parser.parse_args()
 
     tiles_dir = Path(args.t)
@@ -27,9 +29,19 @@ if __name__ == "__main__":
     matching = [scene in marida_scenes for scene in tiles_scenes]
     tiles = list(compress(tiles, matching))
 
-    print("Found {} matching tiles: {}".format(len(tiles), [tile.stem for tile in tiles]))
-
     output_dir = Path(args.o)
+
+    if not args.overwrite:
+        print("Looking for already processed scenes...")
+        existing_tiles = set([path.stem for path in output_dir.iterdir()])
+        remaining_tiles = []
+        for tile in tiles:
+            if tile.stem in existing_tiles:
+                continue
+            remaining_tiles.append(tile)
+        tiles = remaining_tiles
+
+    print("Found {} matching tiles: {}".format(len(tiles), [tile.stem for tile in tiles]))
 
     for tile in tiles:
         scene = marida_dir / "shapefiles" / (get_matching_marida_target(tile.stem) + ".shp")
@@ -37,8 +49,11 @@ if __name__ == "__main__":
 
         pre_process_tile(output_dir, tile.stem, tiles_dir, patches=(20, 20), roi=roi)
         pre_processing_visualizations(output_dir / tile.stem)
-        outliers_df = post_process_patches(output_dir / tile.stem)
+        outliers_df = post_process_patches(output_dir / tile.stem, outliers_keys=("GLOBAL_OUTLIERS", ))
         post_processing_visualizations(output_dir / tile.stem)
+
+        if outliers_df is None:
+            continue
 
         marida_df = load_target(scene)
         aligned_df = align(outliers_df, marida_df)
